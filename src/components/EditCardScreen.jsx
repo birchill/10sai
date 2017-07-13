f05305c796793427ae6b9963835b86f9d3f2f7fa
Brian Birtles <birtles@gmail.com>
2017-07-13 18:47:14 +0900

    Focus search box on loading add form

--- src/components/EditCardScreen.jsx
+++ src/components/EditCardScreen.jsx
@@ -4,59 +4,103 @@ import PropTypes from 'prop-types';
 import CardFaceInput from './CardFaceInput.jsx';
 import Link from './Link.jsx';
 
-function EditCardScreen(props) {
-  return (
-    <section
-      className="edit-screen"
-      aria-hidden={!props.active} >
-      <nav className="buttons tool-bar">
-        <div>
-          <input
-            className="delete -icon -delete -link"
-            type="button"
-            value="Delete" />
-        </div>
-        <div className="-center">
-          <input
-            className="submit -primary -icon -plus"
-            type="submit"
-            value={props.card ? 'OK' : 'Add'} />
-        </div>
-        <div>
-          <Link
-            href="/"
-            className="close-button"
-            direction="backwards">Close</Link>
-        </div>
-      </nav>
-      <form className="form edit-form" autoComplete="off">
-        <div className="search-box">
+export class EditCardScreen extends React.Component {
+  static get propTypes() {
+    return {
+      active: PropTypes.bool.isRequired,
+      card: PropTypes.string,
+    };
+  }
+
+  constructor(props) {
+    super(props);
+    this.assignSearchBox = elem => { this.searchBox = elem; };
+  }
+
+  componentDidMount() {
+    if (this.props.active) this.activate();
+  }
+
+  componentDidUpdate(previousProps) {
+    if (previousProps.active === this.props.active) {
+      return;
+    }
+
+    if (this.props.active) {
+      this.activate();
+    } else {
+      this.deactivate();
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.props.active) this.deactivate();
+  }
+
+  activate() {
+    this.previousFocus = document.activeElement;
+    if (this.searchBox) {
+      this.searchBox.focus();
+    }
+  }
+
+  deactivate() {
+    if (this.previousFocus) {
+      this.previousFocus.focus();
+      this.previousFocus = undefined;
+    }
+  }
+
+  render() {
+    return (
+      <section
+        className="edit-screen"
+        aria-hidden={!this.props.active} >
+        <nav className="buttons tool-bar">
+          <div>
+            <input
+              className="delete -icon -delete -link"
+              type="button"
+              value="Delete" />
+          </div>
+          <div className="-center">
+            <input
+              className="submit -primary -icon -plus"
+              type="submit"
+              value={this.props.card ? 'OK' : 'Add'} />
+          </div>
+          <div>
+            <Link
+              href="/"
+              className="close-button"
+              direction="backwards">Close</Link>
+          </div>
+        </nav>
+        <form className="form edit-form" autoComplete="off">
+          <div className="search-box">
+            <input
+              type="text"
+              placeholder="Lookup"
+              className="text-box -compact -rounded -search"
+              ref={this.assignSearchBox} />
+          </div>
+          <CardFaceInput
+            name="prompt"
+            className="-textpanel -large"
+            placeholder="Prompt"
+            required />
+          <CardFaceInput
+            name="answer"
+            className="-textpanel -large"
+            placeholder="Answer" />
           <input
             type="text"
-            placeholder="Lookup"
-            className="text-box -compact -rounded -search" />
-        </div>
-        <CardFaceInput
-          name="prompt"
-          className="-textpanel -large"
-          placeholder="Prompt"
-          required />
-        <CardFaceInput
-          name="answer"
-          className="-textpanel -large"
-          placeholder="Answer" />
-        <input
-          type="text"
-          placeholder="Keywords"
-          className="-textpanel -compact -yellow" />
-      </form>
-    </section>
-  );
+            placeholder="Keywords"
+            className="-textpanel -compact -yellow" />
+        </form>
+      </section>
+    );
+  }
 }
-
-EditCardScreen.propTypes = {
-  active: PropTypes.bool.isRequired,
-  card: PropTypes.string,
-};
 
 export default EditCardScreen;
