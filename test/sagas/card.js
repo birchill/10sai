51ceea5ffa32e09e61feec1a636ef8c4e03f25e6
Brian Birtles <birtles@gmail.com>
2017-07-30 10:06:33 +0900

    Switch order of card sagas tests to match saga implementation order

--- test/sagas/card.js
+++ test/sagas/card.js
@@ -2,78 +2,8 @@
 /* eslint arrow-body-style: [ 'off' ] */
 
 import { expectSaga } from 'redux-saga-test-plan';
-import { saveCard as saveCardSaga,
-         navigate as navigateSaga } from '../../src/sagas/card';
-
-const saveCard = card => ({
-  type: 'SAVE_CARD',
-  card,
-});
-
-describe('sagas:card saveCard', () => {
-  it('saves the card', () => {
-    const cardStore = {
-      putCard: card => card,
-    };
-    const card = { question: 'yer' };
-
-    return expectSaga(saveCardSaga, cardStore, saveCard(card))
-      .call([ cardStore, 'putCard' ], card)
-      .put({ type: 'FINISH_SAVE_CARD', card })
-      .run();
-  });
-
-  it('reports the ID of the saved card', () => {
-    const cardStore = {
-      putCard: card => ({ ...card, _id: 'generated-id' }),
-    };
-    const card = { question: 'yer' };
-
-    return expectSaga(saveCardSaga, cardStore, saveCard(card))
-      .call([ cardStore, 'putCard' ], card)
-      .put({ type: 'FINISH_SAVE_CARD',
-             card: { ...card, _id: 'generated-id' } })
-      .run();
-  });
-
-  it('updates the history so a new card is previous in history', () => {
-    const cardStore = {
-      putCard: card => ({ ...card, _id: '1234' }),
-    };
-    const card = { question: 'yer' };
-
-    return expectSaga(saveCardSaga, cardStore, saveCard(card))
-      .call([ cardStore, 'putCard' ], card)
-      .put({ type: 'FINISH_SAVE_CARD',
-             card: { ...card, _id: '1234' } })
-      .put({ type: 'INSERT_HISTORY', url: '/cards/1234' })
-      .run();
-  });
-
-  it('does not update history if the card is not new', () => {
-    const cardStore = { putCard: card => card };
-    const card = { question: 'yer', _id: '1234' };
-
-    return expectSaga(saveCardSaga, cardStore, saveCard(card))
-      .call([ cardStore, 'putCard' ], card)
-      .put({ type: 'FINISH_SAVE_CARD', card })
-      .not.put({ type: 'INSERT_HISTORY', url: '/cards/1234' })
-      .run();
-  });
-
-  it('dispatches a failed action when the card cannot be saved', () => {
-    const error = { status: 404, name: 'not_found' };
-    const cardStore = {
-      putCard: () => new Promise((resolve, reject) => { reject(error); })
-    };
-    const card = { question: 'yer' };
-
-    return expectSaga(saveCardSaga, cardStore, saveCard(card))
-      .call([ cardStore, 'putCard' ], card)
-      .put({ type: 'FAIL_SAVE_CARD', error })
-      .run();
-  });
-});
+import { navigate as navigateSaga,
+         saveCard as saveCardSaga } from '../../src/sagas/card';
 
 const navigateWithURL = url => ({
   type: 'NAVIGATE',
@@ -144,6 +74,76 @@ describe('sagas:card navigate', () => {
       .put({ type: 'LOAD_CARD', card: '123' })
       .call([ cardStore, 'getCard' ], '123')
       .put({ type: 'FAIL_LOAD_CARD', error })
+      .run();
+  });
+});
+
+const saveCard = card => ({
+  type: 'SAVE_CARD',
+  card,
+});
+
+describe('sagas:card saveCard', () => {
+  it('saves the card', () => {
+    const cardStore = {
+      putCard: card => card,
+    };
+    const card = { question: 'yer' };
+
+    return expectSaga(saveCardSaga, cardStore, saveCard(card))
+      .call([ cardStore, 'putCard' ], card)
+      .put({ type: 'FINISH_SAVE_CARD', card })
+      .run();
+  });
+
+  it('reports the ID of the saved card', () => {
+    const cardStore = {
+      putCard: card => ({ ...card, _id: 'generated-id' }),
+    };
+    const card = { question: 'yer' };
+
+    return expectSaga(saveCardSaga, cardStore, saveCard(card))
+      .call([ cardStore, 'putCard' ], card)
+      .put({ type: 'FINISH_SAVE_CARD',
+             card: { ...card, _id: 'generated-id' } })
+      .run();
+  });
+
+  it('updates the history so a new card is previous in history', () => {
+    const cardStore = {
+      putCard: card => ({ ...card, _id: '1234' }),
+    };
+    const card = { question: 'yer' };
+
+    return expectSaga(saveCardSaga, cardStore, saveCard(card))
+      .call([ cardStore, 'putCard' ], card)
+      .put({ type: 'FINISH_SAVE_CARD',
+             card: { ...card, _id: '1234' } })
+      .put({ type: 'INSERT_HISTORY', url: '/cards/1234' })
+      .run();
+  });
+
+  it('does not update history if the card is not new', () => {
+    const cardStore = { putCard: card => card };
+    const card = { question: 'yer', _id: '1234' };
+
+    return expectSaga(saveCardSaga, cardStore, saveCard(card))
+      .call([ cardStore, 'putCard' ], card)
+      .put({ type: 'FINISH_SAVE_CARD', card })
+      .not.put({ type: 'INSERT_HISTORY', url: '/cards/1234' })
+      .run();
+  });
+
+  it('dispatches a failed action when the card cannot be saved', () => {
+    const error = { status: 404, name: 'not_found' };
+    const cardStore = {
+      putCard: () => new Promise((resolve, reject) => { reject(error); })
+    };
+    const card = { question: 'yer' };
+
+    return expectSaga(saveCardSaga, cardStore, saveCard(card))
+      .call([ cardStore, 'putCard' ], card)
+      .put({ type: 'FAIL_SAVE_CARD', error })
       .run();
   });
 });
