eb3c7e4f999a176a51d4ebada3f2afb8baee6a44
Brian Birtles <birtles@gmail.com>
2017-08-07 11:28:13 +0900

    Add disabled integration test

--- test/integration/edit.js
+++ test/integration/edit.js
@@ -5,6 +5,8 @@ import { expectSaga } from 'redux-saga-test-plan';
 import reducer from '../../src/reducers/index';
 import editSagas from '../../src/sagas/edit';
 import * as editActions from '../../src/actions/edit';
+// XXX Re-enable the following
+// import * as routeActions from '../../src/actions/route';
 import EditState from '../../src/edit-states';
 
 describe('integration:edit', () => {
@@ -41,6 +43,42 @@ describe('integration:edit', () => {
   });
 
   it('should allow loading, editing, and saving a new card', () => {
+    /*
+    const originalCard = { _id: 'abc', prompt: 'Prompt', answer: 'Answer' };
+    const cardStore = {
+      putCard: card => card,
+      getCard: () => originalCard,
+    };
+    const formId = 18;
+
+    let initialState = reducer(undefined, {});
+
+    return expectSaga(editSagas, cardStore)
+      .withReducer(reducer)
+      .dispatch(routeActions.navigate('/cards/123'))
+      // XXX Need to delay the following until the card has loaded
+      .dispatch(editActions.editCard('123', { prompt: 'Updated prompt'}))
+      // XXX Need to delay the following until the above has completed
+      .dispatch(editActions.saveEditCard('123'))
+      .hasFinalState({
+        // XXX Need to reflect updated route state here
+        ...initialState,
+        edit: {
+          forms: {
+            active: {
+              formId: '123',
+              editState: EditState.OK,
+              card: {
+                _id: '123',
+                prompt: 'Updated prompt',
+                answer: 'Answer',
+              }
+            }
+          }
+        }
+      })
+      .run();
+    */
   });
 
   it('should allow editing while still saving', () => {
