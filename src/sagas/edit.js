ea023510541bc52266fa35081a8a7afc3b972fe6
Brian Birtles <birtles@gmail.com>
2017-08-15 21:27:19 +0900

    Add as-yet unused saga-based approach to auto-saving
    Hopefully this ends up simplifying the components so they don't need to
    manage setTimeout handles and so that auto-save tasks are actually
    cancelled.
    
    The idea is that when you click "Close" or "Add" we'll intercept the
    Link's onClick and dispatch a SAVE_EDIT_CARD with the NAVIGATE action
    stuffed into the SAVE_EDIT_CARD action's onSuccess field.
    
    This should also be more testable and a little more consistent (instead
    of sometimes using Promises in some places etc.)

--- src/sagas/edit.js
+++ src/sagas/edit.js
@@ -1,4 +1,5 @@
-import { takeEvery, call, put, select } from 'redux-saga/effects';
+import { call, delay, fork, put, race, select, spawn, take, takeEvery }
+       from 'redux-saga/effects';
 import { routeFromURL, routeFromPath, URLFromRoute } from '../router';
 import * as editActions from '../actions/edit';
 import EditState from '../edit-states';
@@ -102,6 +103,97 @@ export function* saveEditCard(cardStore, action) {
   } catch (error) {
     yield put(editActions.failSaveCard(action.formId, error));
     saveEditCardPromiseFns.reject(error);
+  }
+}
+
+const SAVE_DELAY = 2000;
+
+function* save(cardStore, formId, card) {
+  try {
+    const savedCard = yield call([ cardStore, 'putCard' ], card);
+    yield put(editActions.finishSaveCard(formId, savedCard));
+
+    // If it is a new card, update the URL.
+    if (!card._id) {
+      const editURL = URLFromRoute({ screen: 'edit-card',
+                                     card: savedCard._id });
+      yield put({ type: 'UPDATE_URL', url: editURL });
+    }
+
+    return savedCard._id;
+  } catch (error) {
+    yield put(editActions.failSaveCard(formId, error));
+    throw error;
+  }
+}
+
+function* autoSave(cardStore, formId, card) {
+  // Debounce -- we allow this part of the task to be cancelled
+  // eslint-disable-next-line no-unused-vars
+  const { wait, cancel } = yield race({
+    wait: call(delay, SAVE_DELAY),
+    cancel: take('CANCEL_AUTO_SAVE'),
+  });
+
+  if (cancel) {
+    return formId;
+  }
+
+  // The remaining steps should not be cancelled since otherwise we risk
+  // writing the card twice with different IDs.
+  return yield save(cardStore, formId, card);
+}
+
+// XXX
+// eslint-disable-next-line no-unused-vars
+function* watchCardEdits(cardStore) {
+  let autoSaveTask;
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const action = yield take([ 'EDIT_CARD', 'SAVE_EDIT_CARD' ]);
+
+    const activeRecord = yield select(getActiveRecord);
+    // In future we'll probably need to look through the different forms
+    // to find the correct one, but for now this should hold.
+    console.assert(activeRecord.formId === action.formId,
+                   'Active record mismatch');
+
+    // Check if anything needs saving
+    if (activeRecord.editState !== EditState.DIRTY) {
+      if (action.onSuccess) {
+        yield spawn(put, action.onSuccess);
+      }
+      continue;
+    }
+
+    let id = action.formId;
+    // If there is an auto save in progress, cancel it.
+    if (autoSaveTask) {
+      if (autoSaveTask.isRunning()) {
+        yield put('CANCEL_AUTO_SAVE');
+      }
+      // Get the possibly updated card ID.
+      try {
+        id = yield autoSaveTask.done;
+      } catch (error) {
+        // If the previous auto-save failed, just ignore it. It will have
+        // dispatched a suitable error action.
+      }
+    }
+    autoSaveTask = undefined;
+
+    if (action.type === 'EDIT_CARD') {
+      autoSaveTask = yield fork(autoSave, cardStore, id, activeRecord.card);
+    } else if (action.type === 'SAVE_EDIT_CARD') {
+      try {
+        yield save(cardStore, id, activeRecord.card);
+        if (action.onSuccess) {
+          yield spawn(put, action.onSuccess);
+        }
+      } catch (error) {
+        // Don't do anything, but don't trigger the onSuccess action.
+      }
+    }
   }
 }
 
