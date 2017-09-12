43dd03220d1f93568f563b313c7caa65c9ea7a5a
Brian Birtles <birtles@gmail.com>
2017-09-12 15:43:04 +1000

    Convert most CardStore-sync tests to async/await

--- test/CardStore-sync.js
+++ test/CardStore-sync.js
@@ -47,110 +47,116 @@ describe('CardStore remote sync', () => {
       throw failedAssertion;
     }
 
-    return Promise.all([ subject.destroy(), testRemote.destroy() ]);
+    return Promise.all([subject.destroy(), testRemote.destroy()]);
   });
 
-  it('allows setting a remote sync server', () => {
-    return subject.setSyncServer(testRemote)
-      .then(() => {
-        assert.isOk(subject.getSyncServer());
-      });
+  it('allows setting a remote sync server', async () => {
+    await subject.setSyncServer(testRemote);
+    assert.isOk(subject.getSyncServer());
   });
 
-  it('rejects for an invalid sync server', () => {
-    return subject.setSyncServer('http://not.found/')
-      .then(() => {
-        assert.fail('Failed to reject invalid server');
-      })
-      .catch(err => {
-        assert.oneOf(err.code, [ 'ENOTFOUND', 'ENOENT', 'ECONNREFUSED' ],
-                     'Expected error for inaccessible server');
-      });
+  it('rejects for an invalid sync server', async () => {
+    try {
+      await subject.setSyncServer('http://not.found/');
+      assert.fail('Failed to reject invalid server');
+    } catch (err) {
+      assert.oneOf(
+        err.code,
+        ['ENOTFOUND', 'ENOENT', 'ECONNREFUSED'],
+        'Expected error for inaccessible server'
+      );
+    }
   });
 
   it('reports an error for an invalid sync server', done => {
-    subject.setSyncServer('http://not.found/',
-      { onError:
-          err => {
-            assert.oneOf(err.code, [ 'ENOTFOUND', 'ENOENT', 'ECONNREFUSED' ],
-                        'Expected error for inaccessible server');
-            done();
-          },
+    subject
+      .setSyncServer('http://not.found/', {
+        onError: err => {
+          assert.oneOf(
+            err.code,
+            ['ENOTFOUND', 'ENOENT', 'ECONNREFUSED'],
+            'Expected error for inaccessible server'
+          );
+          done();
+        },
       })
-      .catch(() => { /* Ok */ });
-  });
-
-  it('rejects a non-http/https database', () => {
-    return subject.setSyncServer('irc://irc.mozilla.org')
-      .then(() => {
-        assert.fail('Failed to reject invalid server');
-      })
-      .catch(err => {
-        assert.strictEqual(err.code, 'INVALID_SERVER');
+      .catch(() => {
+        /* Ok */
       });
   });
 
-  it('rejects a non-PouchDB object', () => {
-    return subject.setSyncServer(new Date())
-      .then(() => {
-        assert.fail('Failed to reject invalid server');
-      })
-      .catch(err => {
-        assert.strictEqual(err.code, 'INVALID_SERVER');
-      });
+  it('rejects a non-http/https database', async () => {
+    try {
+      await subject.setSyncServer('irc://irc.mozilla.org');
+      assert.fail('Failed to reject invalid server');
+    } catch (err) {
+      assert.strictEqual(err.code, 'INVALID_SERVER');
+    }
   });
 
-  it('allows clearing the sync server using null', () => {
-    return subject.setSyncServer(testRemote)
-      .then(() => subject.setSyncServer(null))
-      .then(() => {
-        assert.strictEqual(subject.getSyncServer(), undefined);
-      });
+  it('rejects a non-PouchDB object', async () => {
+    try {
+      await subject.setSyncServer(new Date());
+      assert.fail('Failed to reject invalid server');
+    } catch (err) {
+      assert.strictEqual(err.code, 'INVALID_SERVER');
+    }
   });
 
-  it('allows clearing the sync server using undefined', () => {
-    return subject.setSyncServer(testRemote)
-      .then(() => subject.setSyncServer())
-      .then(() => {
-        assert.strictEqual(subject.getSyncServer(), undefined);
-      });
+  it('allows clearing the sync server using null', async () => {
+    await subject.setSyncServer(testRemote);
+    await subject.setSyncServer(null);
+    assert.strictEqual(subject.getSyncServer(), undefined);
   });
 
-  it('allows clearing the sync server using an empty name', () => {
-    return subject.setSyncServer(testRemote)
-      .then(() => subject.setSyncServer(''))
-      .then(() => {
-        assert.strictEqual(subject.getSyncServer(), undefined);
-      });
+  it('allows clearing the sync server using undefined', async () => {
+    await subject.setSyncServer(testRemote);
+    await subject.setSyncServer();
+    assert.strictEqual(subject.getSyncServer(), undefined);
   });
 
-  it('allows clearing the sync server using an entirely whitespace name',
-  () => {
-    return subject.setSyncServer(testRemote)
-      .then(() => subject.setSyncServer('  \n '))
-      .then(() => {
-        assert.strictEqual(subject.getSyncServer(), undefined);
-      });
+  it('allows clearing the sync server using an empty name', async () => {
+    await subject.setSyncServer(testRemote);
+    await subject.setSyncServer('');
+    assert.strictEqual(subject.getSyncServer(), undefined);
+  });
+
+  it('allows clearing the sync server using an entirely whitespace name', async () => {
+    await subject.setSyncServer(testRemote);
+    await subject.setSyncServer('  \n ');
+    assert.strictEqual(subject.getSyncServer(), undefined);
   });
 
   it('downloads existing cards on the remote server', done => {
-    const firstCard =  { question: 'Question 1',
-                         answer:   'Answer 1',
-                         _id: CardStore.generateCardId() };
-    const secondCard = { question: 'Question 2',
-                         answer:   'Answer 2',
-                         _id: CardStore.generateCardId() };
+    const firstCard = {
+      question: 'Question 1',
+      answer: 'Answer 1',
+      _id: CardStore.generateCardId(),
+    };
+    const secondCard = {
+      question: 'Question 2',
+      answer: 'Answer 2',
+      _id: CardStore.generateCardId(),
+    };
 
-    const expectedCards = [ firstCard, secondCard ];
+    const expectedCards = [firstCard, secondCard];
 
-    subject.changes.on('change', wrapAssertingFunction(info => {
-      assert.deepEqual(info.doc, expectedCards.shift());
-    }));
+    subject.changes.on(
+      'change',
+      wrapAssertingFunction(info => {
+        assert.deepEqual(info.doc, expectedCards.shift());
+      })
+    );
 
-    testRemote.put(firstCard)
-      .then(result => { firstCard._rev = result.rev; })
+    testRemote
+      .put(firstCard)
+      .then(result => {
+        firstCard._rev = result.rev;
+      })
       .then(() => testRemote.put(secondCard))
-      .then(result => { secondCard._rev = result.rev; })
+      .then(result => {
+        secondCard._rev = result.rev;
+      })
       .then(() => subject.setSyncServer(testRemote))
       .then(() => {
         (function waitForUpdates() {
@@ -163,28 +169,33 @@ describe('CardStore remote sync', () => {
       });
   });
 
-  it('disassociates from previous remote sync server when a new one is set',
-  () => {
-    const card = { question: 'Question',
-                   answer:   'Answer',
-                   _id: CardStore.generateCardId() };
+  it('disassociates from previous remote sync server when a new one is set', async () => {
+    const card = {
+      question: 'Question',
+      answer: 'Answer',
+      _id: CardStore.generateCardId(),
+    };
 
     const alternateRemote = new PouchDB('cards_remote_2', { db: memdown });
 
-    subject.changes.on('change', wrapAssertingFunction(() => {
-      assert.fail('Did not expect update to be called on the previous remote');
-    }));
+    subject.changes.on(
+      'change',
+      wrapAssertingFunction(() => {
+        assert.fail(
+          'Did not expect update to be called on the previous remote'
+        );
+      })
+    );
 
-    return subject.setSyncServer(testRemote)
-      .then(() => subject.setSyncServer(alternateRemote))
-      .then(() => testRemote.put(card))
-      .then(() => waitForEvents(20))
-      .then(() => {
-        alternateRemote.destroy();
-      });
+    await subject.setSyncServer(testRemote);
+    await subject.setSyncServer(alternateRemote);
+    await testRemote.put(card);
+
+    await waitForEvents(20);
+    await alternateRemote.destroy();
   });
 
-  it('does not report events from the old remote', () => {
+  it('does not report events from the old remote', async () => {
     const alternateRemote = new PouchDB('cards_remote_2', { db: memdown });
     const callbacks = {
       onIdle: wrapAssertingFunction(() => {
@@ -192,28 +203,28 @@ describe('CardStore remote sync', () => {
       }),
     };
 
-    return subject.setSyncServer(testRemote, callbacks)
-      .then(() => subject.setSyncServer(alternateRemote))
-      .then(() => waitForEvents(20))
-      .then(() => {
-        alternateRemote.destroy();
-      });
+    await subject.setSyncServer(testRemote, callbacks);
+    await subject.setSyncServer(alternateRemote);
+
+    await waitForEvents(20);
+    await alternateRemote.destroy();
   });
 
-  it('uploads existing local cards', () => {
+  it('uploads existing local cards', async () => {
     let resolveIdle;
-    const idlePromise = new Promise(resolve => { resolveIdle = resolve; });
+    const idlePromise = new Promise(resolve => {
+      resolveIdle = resolve;
+    });
 
-    return subject.putCard({ question: 'Question 1', answer: 'Answer 1' })
-      .then(() =>
-        subject.putCard({ question: 'Question 2', answer: 'Answer 2' }))
-      .then(() => subject.setSyncServer(testRemote,
-                                        { onIdle: () => resolveIdle() }))
-      .then(() => idlePromise)
-      .then(() => testRemote.allDocs({ include_docs: true, descending: true }))
-      .then(remoteCards => {
-        assert.strictEqual(remoteCards.rows.length, 2);
-      });
+    await subject.putCard({ question: 'Question 1', answer: 'Answer 1' });
+    await subject.putCard({ question: 'Question 2', answer: 'Answer 2' });
+    await subject.setSyncServer(testRemote, { onIdle: () => resolveIdle() });
+    await idlePromise;
+    const remoteCards = await testRemote.allDocs({
+      include_docs: true,
+      descending: true,
+    });
+    assert.strictEqual(remoteCards.rows.length, 2);
   });
 
   it('reports when download starts and stops', done => {
@@ -221,11 +232,14 @@ describe('CardStore remote sync', () => {
       onActive: wrapAssertingFunction(info => {
         assert.strictEqual(info.direction, 'pull');
         done();
-      }) });
+      }),
+    });
 
-    testRemote.put({ question: 'Question',
-                     answer: 'Answer',
-                     _id: CardStore.generateCardId() });
+    testRemote.put({
+      question: 'Question',
+      answer: 'Answer',
+      _id: CardStore.generateCardId(),
+    });
   });
 
   it('reports when uploads starts and stops', done => {
@@ -233,131 +247,169 @@ describe('CardStore remote sync', () => {
       onActive: wrapAssertingFunction(info => {
         assert.strictEqual(info.direction, 'push');
         done();
-      }) });
+      }),
+    });
 
     subject.putCard({ question: 'Question', answer: 'Answer' });
   });
 
-  it('reports sync progress on initial download', () => {
+  it('reports sync progress on initial download', async () => {
     const numCards = 5;
     const docs = [];
     for (let i = 0; i < numCards; i++) {
-      docs.push({ question: `Question ${i + 1}`,
-                  answer: `Answer ${i + 1}`,
-                  _id: CardStore.generateCardId() });
+      docs.push({
+        question: `Question ${i + 1}`,
+        answer: `Answer ${i + 1}`,
+        _id: CardStore.generateCardId(),
+      });
     }
 
     let resolveAllDone;
-    const allDone = new Promise(resolve => { resolveAllDone = resolve; });
+    const allDone = new Promise(resolve => {
+      resolveAllDone = resolve;
+    });
 
     const allChanges = [];
-    return testRemote.bulkDocs(docs)
-      .then(() =>
-        subject.setSyncServer(testRemote, {
-          onChange: changes => allChanges.push(changes),
-          onIdle: () => resolveAllDone(),
-          batchSize: 2,
-        }))
-      .then(() => allDone)
-      .then(() => {
-        assert.strictEqual(allChanges.length, 3,
-                           'Should be three batches of changes');
-        assert.deepEqual(allChanges.map(change => change.progress),
-                         [ 0.4, 0.8, 1.0 ], 'Each batch has expected progress');
-      });
+    await testRemote.bulkDocs(docs);
+    await subject.setSyncServer(testRemote, {
+      onChange: changes => allChanges.push(changes),
+      onIdle: () => resolveAllDone(),
+      batchSize: 2,
+    });
+    await allDone;
+
+    assert.strictEqual(
+      allChanges.length,
+      3,
+      'Should be three batches of changes'
+    );
+    assert.deepEqual(
+      allChanges.map(change => change.progress),
+      [0.4, 0.8, 1.0],
+      'Each batch has expected progress'
+    );
   });
 
-  it('reports sync progress on initial upload', () => {
+  it('reports sync progress on initial upload', async () => {
     const numCards = 5;
     const putPromises = [];
     for (let i = 0; i < numCards; i++) {
-      putPromises.push(subject.putCard({ question: `Question ${i + 1}`,
-                                         answer: `Answer ${i + 1}` }));
+      putPromises.push(
+        subject.putCard({
+          question: `Question ${i + 1}`,
+          answer: `Answer ${i + 1}`,
+        })
+      );
     }
 
     let resolveAllDone;
-    const allDone = new Promise(resolve => { resolveAllDone = resolve; });
+    const allDone = new Promise(resolve => {
+      resolveAllDone = resolve;
+    });
 
     const allChanges = [];
-    return Promise.all(putPromises)
-      .then(() =>
-        subject.setSyncServer(testRemote, {
-          onChange: changes => allChanges.push(changes),
-          onIdle: () => resolveAllDone(),
-          batchSize: 2,
-        }))
-      .then(() => allDone)
-      .then(() => {
-        assert.strictEqual(allChanges.length, 3,
-                           'Should be three batches of changes');
-        assert.deepEqual(allChanges.map(change => change.progress),
-                         [ 0.4, 0.8, 1.0 ], 'Each batch has expected progress');
-      });
+    await Promise.all(putPromises);
+    await subject.setSyncServer(testRemote, {
+      onChange: changes => allChanges.push(changes),
+      onIdle: () => resolveAllDone(),
+      batchSize: 2,
+    });
+    await allDone;
+
+    assert.strictEqual(
+      allChanges.length,
+      3,
+      'Should be three batches of changes'
+    );
+    assert.deepEqual(
+      allChanges.map(change => change.progress),
+      [0.4, 0.8, 1.0],
+      'Each batch has expected progress'
+    );
   });
 
-  it('reports indeterminate progress on balanced bi-directional sync', () => {
+  it('reports indeterminate progress on balanced bi-directional sync', async () => {
     const localCards = 5;
     const putPromises = [];
     for (let i = 0; i < localCards; i++) {
-      putPromises.push(subject.putCard({ question: `Local question ${i + 1}`,
-                                         answer: `Local answer ${i + 1}` }));
+      putPromises.push(
+        subject.putCard({
+          question: `Local question ${i + 1}`,
+          answer: `Local answer ${i + 1}`,
+        })
+      );
     }
 
     const remoteCards = 5;
     const remoteDocs = [];
     for (let i = 0; i < remoteCards; i++) {
-      remoteDocs.push({ question: `Remote question ${i + 1}`,
-                        answer: `Remote answer ${i + 1}`,
-                        _id: CardStore.generateCardId() });
+      remoteDocs.push({
+        question: `Remote question ${i + 1}`,
+        answer: `Remote answer ${i + 1}`,
+        _id: CardStore.generateCardId(),
+      });
     }
 
     let resolveAllDone;
-    const allDone = new Promise(resolve => { resolveAllDone = resolve; });
+    const allDone = new Promise(resolve => {
+      resolveAllDone = resolve;
+    });
 
     const allChanges = [];
-    return Promise.all(putPromises)
-      .then(() => testRemote.bulkDocs(remoteDocs))
-      .then(() =>
-        subject.setSyncServer(testRemote, {
-          onChange: changes => allChanges.push(changes),
-          onIdle: () => resolveAllDone(),
-          batchSize: 2,
-        }))
-      .then(() => allDone)
-      .then(() => {
-        assert.strictEqual(allChanges.length, 6,
-                           'Should be six batches of changes');
-        assert.deepEqual(allChanges.map(change => change.progress),
-                         Array(allChanges.length).fill(null),
-                         'Each batch should have an indeterminate progress');
-      });
+    await Promise.all(putPromises);
+    await testRemote.bulkDocs(remoteDocs);
+    await subject.setSyncServer(testRemote, {
+      onChange: changes => allChanges.push(changes),
+      onIdle: () => resolveAllDone(),
+      batchSize: 2,
+    });
+    await allDone;
+
+    assert.strictEqual(
+      allChanges.length,
+      6,
+      'Should be six batches of changes'
+    );
+    assert.deepEqual(
+      allChanges.map(change => change.progress),
+      Array(allChanges.length).fill(null),
+      'Each batch should have an indeterminate progress'
+    );
   });
 
-  it('reports indeterminate sync progress on subsequent download', () => {
+  it('reports indeterminate sync progress on subsequent download', async () => {
     const numCards = 3;
     const docs = [];
     for (let i = 0; i < numCards; i++) {
-      docs.push({ question: `Question ${i + 1}`,
-                  answer: `Answer ${i + 1}`,
-                  _id: CardStore.generateCardId() });
+      docs.push({
+        question: `Question ${i + 1}`,
+        answer: `Answer ${i + 1}`,
+        _id: CardStore.generateCardId(),
+      });
     }
 
     let resolveIdle;
-    const waitForIdle = new Promise(resolve => { resolveIdle = resolve; });
+    const waitForIdle = new Promise(resolve => {
+      resolveIdle = resolve;
+    });
 
     const allChanges = [];
-    return subject.setSyncServer(testRemote,
-      { onChange: changes => allChanges.push(changes),
-        onIdle: () => resolveIdle(),
-        batchSize: 2 })
-      .then(() => waitForIdle)
-      .then(() => testRemote.bulkDocs(docs))
-      .then(() => new Promise(resolve => { resolveIdle = resolve; }))
-      .then(() => {
-        assert.deepEqual(allChanges.map(change => change.progress),
-                         Array(allChanges.length).fill(null),
-                         'Each batch should have an indeterminate progress');
-      });
+    await subject.setSyncServer(testRemote, {
+      onChange: changes => allChanges.push(changes),
+      onIdle: () => resolveIdle(),
+      batchSize: 2,
+    });
+    await waitForIdle;
+    await testRemote.bulkDocs(docs);
+    await new Promise(resolve => {
+      resolveIdle = resolve;
+    });
+
+    assert.deepEqual(
+      allChanges.map(change => change.progress),
+      Array(allChanges.length).fill(null),
+      'Each batch should have an indeterminate progress'
+    );
   });
 
   it('reports an error when the remote server goes offline', () => {
