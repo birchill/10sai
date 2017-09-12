37acd2a2a96d64ec2c09a8ba6352a3bc282b0272
Brian Birtles <birtles@gmail.com>
2017-09-12 14:55:05 +1000

    Convert some CardStore tests to async/await

--- test/CardStore.js
+++ test/CardStore.js
@@ -15,43 +15,39 @@ describe('CardStore', () => {
 
   afterEach('clean up store', () => subject.destroy());
 
-  it('is initially empty', () => {
-    return subject.getCards()
-      .then(cards => {
-        assert.strictEqual(cards.length, 0, 'Length of getCards() result');
-      });
+  it('is initially empty', async () => {
+    const cards = await subject.getCards();
+    assert.strictEqual(cards.length, 0, 'Length of getCards() result');
   });
 
-  it('returns added cards', () => {
-    return subject.putCard({ question: 'Question', answer: 'Answer' })
-      .then(() => subject.getCards())
-      .then(cards => {
-        assert.strictEqual(cards.length, 1, 'Length of getCards() result');
-        assert.strictEqual(cards[0].question, 'Question');
-        assert.strictEqual(cards[0].answer, 'Answer');
-      });
+  it('returns added cards', async () => {
+    await subject.putCard({ question: 'Question', answer: 'Answer' });
+    const cards = await subject.getCards();
+    assert.strictEqual(cards.length, 1, 'Length of getCards() result');
+    assert.strictEqual(cards[0].question, 'Question');
+    assert.strictEqual(cards[0].answer, 'Answer');
   });
 
-  it('returns individual cards', () => {
-    return subject.putCard({ question: 'Question', answer: 'Answer' })
-      .then(card => subject.getCard(card._id))
-      .then(card => {
-        assert.strictEqual(card.question, 'Question');
-        assert.strictEqual(card.answer, 'Answer');
-      });
+  it('returns individual cards', async () => {
+    let card = await subject.putCard({
+      question: 'Question',
+      answer: 'Answer',
+    });
+    card = await subject.getCard(card._id);
+    assert.strictEqual(card.question, 'Question');
+    assert.strictEqual(card.answer, 'Answer');
   });
 
-  it('does not return non-existent cards', () => {
-    return subject.getCard('abc')
-      .then(() => {
-        assert.fail('Should have reported an error for non-existent card');
-      })
-      .catch(err => {
-        assert.strictEqual(err.status, 404);
-        assert.strictEqual(err.name, 'not_found');
-        assert.strictEqual(err.message, 'missing');
-        assert.strictEqual(err.reason, 'missing');
-      });
+  it('does not return non-existent cards', async () => {
+    try {
+      await subject.getCard('abc');
+      assert.fail('Should have reported an error for non-existent card');
+    } catch (err) {
+      assert.strictEqual(err.status, 404);
+      assert.strictEqual(err.name, 'not_found');
+      assert.strictEqual(err.message, 'missing');
+      assert.strictEqual(err.reason, 'missing');
+    }
   });
 
   it('generates unique ascending IDs', () => {
@@ -63,224 +59,250 @@ describe('CardStore', () => {
     }
   });
 
-  it('returns added cards in order', () => {
-    let id1;
-    let id2;
+  it('returns added cards in order', async () => {
+    const card1 = await subject.putCard({ question: 'Q1', answer: 'A1' });
+    const card2 = await subject.putCard({ question: 'Q2', answer: 'A2' });
 
-    return subject.putCard({ question: 'Q1', answer: 'A1' })
-      .then(card => { id1 = card._id; })
-      .then(() => subject.putCard({ question: 'Q2', answer: 'A2' }))
-      .then(card => { id2 = card._id; })
-      .then(() => subject.getCards())
-      .then(cards => {
-        // Sanity check
-        assert.notStrictEqual(id1, id2, 'Card IDs are unique');
+    const cards = await subject.getCards();
+    // Sanity check
+    assert.notStrictEqual(card1._id, card2._id, 'Card IDs are unique');
 
-        assert.strictEqual(cards.length, 2, 'Expected no. of cards');
-        assert.strictEqual(cards[0]._id, id2,
-                           'Card added last is returned first');
-        assert.strictEqual(cards[1]._id, id1,
-                           'Card added first is returned last');
-      });
+    assert.strictEqual(cards.length, 2, 'Expected no. of cards');
+    assert.strictEqual(
+      cards[0]._id,
+      card2._id,
+      'Card added last is returned first'
+    );
+    assert.strictEqual(
+      cards[1]._id,
+      card1._id,
+      'Card added first is returned last'
+    );
   });
 
-  it('reports added cards', () => {
-    let addedCard;
+  it('reports added cards', async () => {
     let updateInfo;
+    subject.changes.on('change', info => {
+      updateInfo = info;
+    });
 
-    subject.changes.on('change', info => { updateInfo = info; });
+    const addedCard = await subject.putCard({ question: 'Q1', answer: 'A1' });
+    // Wait for a few rounds of events so the update can take place
+    await waitForEvents(3);
 
-    return subject.putCard({ question: 'Q1', answer: 'A1' })
-      .then(card => { addedCard = card; })
-      // Wait for a few rounds of events so the update can take place
-      .then(() => waitForEvents(3))
-      .then(() => {
-        assert.isOk(updateInfo, 'Change was recorded');
-        assert.strictEqual(updateInfo.id, addedCard._id,
-                           'Reported change has correct ID');
-      });
+    assert.isOk(updateInfo, 'Change was recorded');
+    assert.strictEqual(
+      updateInfo.id,
+      addedCard._id,
+      'Reported change has correct ID'
+    );
   });
 
-  it('does not return deleted cards', () => {
-    return subject.putCard({ question: 'Question', answer: 'Answer' })
-      .then(card => subject.deleteCard(card))
-      .then(() => subject.getCards())
-      .then(cards => {
-        assert.strictEqual(cards.length, 0, 'Length of getCards() result');
-      });
+  it('does not return deleted cards', async () => {
+    const card = await subject.putCard({
+      question: 'Question',
+      answer: 'Answer',
+    });
+    await subject.deleteCard(card);
+
+    const cards = await subject.getCards();
+
+    assert.strictEqual(cards.length, 0, 'Length of getCards() result');
   });
 
-  it('does not return individual deleted cards', () => {
-    let id;
+  it('does not return individual deleted cards', async () => {
+    const card = await subject.putCard({
+      question: 'Question',
+      answer: 'Answer',
+    });
+    const id = card._id;
+    await subject.deleteCard(card);
 
-    return subject.putCard({ question: 'Question', answer: 'Answer' })
-      .then(card => { id = card._id; return card; })
-      .then(card => subject.deleteCard(card))
-      .then(() => subject.getCard(id))
-      .then(() => {
-        assert.fail('Should have reported an error for non-existent card');
-      })
-      .catch(err => {
-        assert.strictEqual(err.status, 404);
-        assert.strictEqual(err.name, 'not_found');
-        assert.strictEqual(err.message, 'missing');
-        assert.strictEqual(err.reason, 'deleted');
-      });
+    try {
+      await subject.getCard(id);
+      assert.fail('Should have reported an error for non-existent card');
+    } catch (err) {
+      assert.strictEqual(err.status, 404);
+      assert.strictEqual(err.name, 'not_found');
+      assert.strictEqual(err.message, 'missing');
+      assert.strictEqual(err.reason, 'deleted');
+    }
   });
 
-  it('deletes the specified card', () => {
-    let firstCard;
-    return subject.putCard({ question: 'Question 1',
-                             answer: 'Answer 1' })
-      .then(card => { firstCard = card; })
-      .then(() => subject.putCard({ question: 'Question 2',
-                                    answer: 'Answer 2' }))
-      .then(() => subject.deleteCard(firstCard))
-      .then(() => subject.getCards())
-      .then(cards => {
-        assert.strictEqual(cards.length, 1, 'Length of getCards() result');
-        assert.strictEqual(cards[0].question, 'Question 2');
-        assert.strictEqual(cards[0].answer, 'Answer 2');
-      });
+  it('deletes the specified card', async () => {
+    const firstCard = await subject.putCard({
+      question: 'Question 1',
+      answer: 'Answer 1',
+    });
+    await subject.putCard({ question: 'Question 2', answer: 'Answer 2' });
+
+    await subject.deleteCard(firstCard);
+
+    const cards = await subject.getCards();
+    assert.strictEqual(cards.length, 1, 'Length of getCards() result');
+    assert.strictEqual(cards[0].question, 'Question 2');
+    assert.strictEqual(cards[0].answer, 'Answer 2');
   });
 
-  it('reports an error when the card to be deleted cannot be found', () => {
-    return subject.deleteCard({ _id: 'abc' })
-      .then(() => {
-        assert.fail('Should have reported an error for missing card');
-      })
-      .catch(err => {
-        assert.strictEqual(err.status, 404);
-        assert.strictEqual(err.name, 'not_found');
-        assert.strictEqual(err.message, 'missing');
-        assert.strictEqual(err.reason, 'deleted');
-      });
+  it('reports an error when the card to be deleted cannot be found', async () => {
+    try {
+      await subject.deleteCard({ _id: 'abc' });
+      assert.fail('Should have reported an error for missing card');
+    } catch (err) {
+      assert.strictEqual(err.status, 404);
+      assert.strictEqual(err.name, 'not_found');
+      assert.strictEqual(err.message, 'missing');
+      assert.strictEqual(err.reason, 'deleted');
+    }
   });
 
-  it('deletes the specified card even when the revision is old', () => {
-    let card;
-    return subject.putCard({ question: 'Question 1',
-                             answer: 'Answer 1' })
-      .then(result => { card = result; })
-      .then(() => subject.putCard({ ...card, question: 'Updated question' }))
-      .then(() => subject.deleteCard(card))
-      .then(() => subject.getCards())
-      .then(cards => {
-        assert.strictEqual(cards.length, 0, 'Length of getCards() result');
-      });
+  it('deletes the specified card even when the revision is old', async () => {
+    const card = await subject.putCard({
+      question: 'Question 1',
+      answer: 'Answer 1',
+    });
+    await subject.putCard({ ...card, question: 'Updated question' });
+
+    await subject.deleteCard(card);
+
+    const cards = await subject.getCards();
+    assert.strictEqual(cards.length, 0, 'Length of getCards() result');
   });
 
-  it('reports deleted cards', () => {
-    let addedCard;
+  it('reports deleted cards', async () => {
     let updateInfo;
+    subject.changes.on('change', info => {
+      updateInfo = info;
+    });
+    const addedCard = await subject.putCard({
+      question: 'Question',
+      answer: 'Answer',
+    });
 
-    subject.changes.on('change', info => { updateInfo = info; });
+    await subject.deleteCard(addedCard);
 
-    return subject.putCard({ question: 'Question', answer: 'Answer' })
-      .then(card => { addedCard = card; })
-      .then(() => subject.deleteCard(addedCard))
-      .then(() => waitForEvents(5))
-      .then(() => {
-        assert.strictEqual(updateInfo.id, addedCard._id,
-                           'Reported change has correct ID');
-        assert.isOk(updateInfo.deleted,
-                    'Reported change is a delete record');
-      });
+    await waitForEvents(5);
+    assert.strictEqual(
+      updateInfo.id,
+      addedCard._id,
+      'Reported change has correct ID'
+    );
+    assert.isOk(updateInfo.deleted, 'Reported change is a delete record');
   });
 
-  it('updates the specified field of cards', () => {
-    return subject.putCard({ question: 'Original question', answer: 'Answer' })
-      .then(card => subject.putCard({ _id: card._id,
-                                      _rev: card._rev,
-                                      question: 'Updated question' }))
-      .then(card => {
-        assert.strictEqual(card.question, 'Updated question');
-        assert.strictEqual(card.answer, 'Answer');
-      })
-      .then(() => subject.getCards())
-      .then(cards => {
-        assert.strictEqual(cards.length, 1, 'Length of getCards() result');
-        assert.strictEqual(cards[0].question, 'Updated question');
-        assert.strictEqual(cards[0].answer, 'Answer');
-      });
+  it('updates the specified field of cards', async () => {
+    let card = await subject.putCard({
+      question: 'Original question',
+      answer: 'Answer',
+    });
+
+    card = await subject.putCard({
+      _id: card._id,
+      _rev: card._rev,
+      question: 'Updated question',
+    });
+
+    assert.strictEqual(card.question, 'Updated question');
+    assert.strictEqual(card.answer, 'Answer');
+
+    const cards = await subject.getCards();
+    assert.strictEqual(cards.length, 1, 'Length of getCards() result');
+    assert.strictEqual(cards[0].question, 'Updated question');
+    assert.strictEqual(cards[0].answer, 'Answer');
   });
 
-  it('updates cards even without a revision', () => {
-    return subject.putCard({ question: 'Original question', answer: 'Answer' })
-      .then(card => subject.putCard({ _id: card._id,
-                                      question: 'Updated question' }))
-      .then(card => {
-        assert.strictEqual(card.question, 'Updated question');
-        assert.strictEqual(card.answer, 'Answer');
-      })
-      .then(() => subject.getCards())
-      .then(cards => {
-        assert.strictEqual(cards.length, 1, 'Length of getCards() result');
-        assert.strictEqual(cards[0].question, 'Updated question');
-        assert.strictEqual(cards[0].answer, 'Answer');
-      });
+  it('updates cards even without a revision', async () => {
+    let card = await subject.putCard({
+      question: 'Original question',
+      answer: 'Answer',
+    });
+
+    card = await subject.putCard({
+      _id: card._id,
+      question: 'Updated question',
+    });
+
+    assert.strictEqual(card.question, 'Updated question');
+    assert.strictEqual(card.answer, 'Answer');
+
+    const cards = await subject.getCards();
+    assert.strictEqual(cards.length, 1, 'Length of getCards() result');
+    assert.strictEqual(cards[0].question, 'Updated question');
+    assert.strictEqual(cards[0].answer, 'Answer');
   });
 
-  it('updates cards even when the revision is old', () => {
-    let oldRevision;
+  it('updates cards even when the revision is old', async () => {
+    let card = await subject.putCard({
+      question: 'Original question',
+      answer: 'Answer',
+    });
+    const oldRevision = card._rev;
+    card = await subject.putCard({
+      _id: card._id,
+      question: 'Updated question',
+    });
 
-    return subject.putCard({ question: 'Original question', answer: 'Answer' })
-      .then(card => {
-        oldRevision = card._rev;
-        return subject.putCard({ _id: card._id,
-                                 question: 'Updated question' });
-      })
-      .then(card => subject.putCard({ _id: card._id,
-                                      _rev: oldRevision,
-                                      answer: 'Updated answer' }))
-      .then(() => subject.getCards())
-      .then(cards => {
-        assert.strictEqual(cards.length, 1, 'Length of getCards() result');
-        assert.strictEqual(cards[0].question, 'Updated question');
-        assert.strictEqual(cards[0].answer, 'Updated answer');
-      });
+    await subject.putCard({
+      _id: card._id,
+      _rev: oldRevision,
+      answer: 'Updated answer',
+    });
+
+    const cards = await subject.getCards();
+    assert.strictEqual(cards.length, 1, 'Length of getCards() result');
+    assert.strictEqual(cards[0].question, 'Updated question');
+    assert.strictEqual(cards[0].answer, 'Updated answer');
   });
 
-  it('returns an error when trying to update a missing card', () => {
-    return subject.putCard({ _id: 'abc', question: 'Question' })
-      .then(() => {
-        assert.fail('Should have reported an error for missing card');
-      })
-      .catch(err => {
-        assert.strictEqual(err.status, 404);
-        assert.strictEqual(err.name, 'not_found');
-        assert.strictEqual(err.message, 'missing');
-      });
+  it('returns an error when trying to update a missing card', async () => {
+    try {
+      await subject.putCard({ _id: 'abc', question: 'Question' });
+      assert.fail('Should have reported an error for missing card');
+    } catch (err) {
+      assert.strictEqual(err.status, 404);
+      assert.strictEqual(err.name, 'not_found');
+      assert.strictEqual(err.message, 'missing');
+    }
   });
 
-  it('returns an error when trying to update a deleted card', () => {
-    let id;
-    return subject.putCard({ question: 'Question', answer: 'Answer' })
-      .then(card => { id = card._id; return card; })
-      .then(card => subject.deleteCard(card))
-      .then(() => subject.putCard({ _id: id, question: 'Updated question' }))
-      .then(() => {
-        assert.fail('Should have reported an error for deleted card');
-      })
-      .catch(err => {
-        assert.strictEqual(err.status, 404);
-        assert.strictEqual(err.name, 'not_found');
-        assert.strictEqual(err.message, 'missing');
-      });
+  it('returns an error when trying to update a deleted card', async () => {
+    const card = await subject.putCard({
+      question: 'Question',
+      answer: 'Answer',
+    });
+    await subject.deleteCard(card);
+
+    try {
+      await subject.putCard({ _id: card._id, question: 'Updated question' });
+
+      assert.fail('Should have reported an error for deleted card');
+    } catch (err) {
+      assert.strictEqual(err.status, 404);
+      assert.strictEqual(err.name, 'not_found');
+      assert.strictEqual(err.message, 'missing');
+    }
   });
 
-  it('reports changes to cards', () => {
+  it('reports changes to cards', async () => {
     const updates = [];
-    subject.changes.on('change', info => { updates.push(info); });
+    subject.changes.on('change', info => {
+      updates.push(info);
+    });
 
-    return subject.putCard({ question: 'Question', answer: 'Answer' })
-      .then(card => subject.putCard({ ...card, question: 'Updated question' }))
-      // Wait for a few rounds of events so the update records can happen
-      .then(() => waitForEvents(3))
-      .then(() => {
-        assert.strictEqual(updates.length, 2,
-                           'Should get two change records: add, update');
-        assert.strictEqual(updates[1].doc.question, 'Updated question');
-      });
+    const card = await subject.putCard({
+      question: 'Question',
+      answer: 'Answer',
+    });
+    await subject.putCard({ ...card, question: 'Updated question' });
+
+    // Wait for a few rounds of events so the update records can happen
+    await waitForEvents(3);
+
+    assert.strictEqual(
+      updates.length,
+      2,
+      'Should get two change records: add, update'
+    );
+    assert.strictEqual(updates[1].doc.question, 'Updated question');
   });
 });
