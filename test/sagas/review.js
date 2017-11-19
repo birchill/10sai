9423f881120cea5d8e439b648444b4aad81e1d60
Brian Birtles <birtles@gmail.com>
2017-11-19 17:31:32 +0900

    Add tests for updateProgress saga

--- test/sagas/review.js
+++ test/sagas/review.js
@@ -1,10 +1,14 @@
 /* global describe, it */
 /* eslint arrow-body-style: [ 'off' ] */
 
+import { assert } from 'chai';
 import { expectSaga } from 'redux-saga-test-plan';
 import * as matchers from 'redux-saga-test-plan/matchers';
 
-import { updateHeap as updateHeapSaga } from '../../src/sagas/review';
+import {
+  updateHeap as updateHeapSaga,
+  updateProgress as updateProgressSaga,
+} from '../../src/sagas/review';
 import * as reviewActions from '../../src/actions/review';
 import reducer from '../../src/reducers/index';
 
@@ -147,13 +151,141 @@ describe('sagas:review updateHeap', () => {
 });
 
 describe('sagas:review updateProgress', () => {
-  /*
-  const cardStore = {
-    updateProgress: () => {},
-  };
-  */
+  const MS_PER_DAY = 1000 * 60 * 60 * 24;
 
-  it('updates the progress of a failed card', async () => {
-    // TODO
+  const getCards = (maxNewCards, maxExistingCards, reviewTime) => {
+    const cards = new Array(Math.max(maxNewCards, maxExistingCards));
+    for (let i = 0; i < cards.length; i++) {
+      const newCard = i < maxNewCards;
+      cards[i] = {
+        question: `Question ${i + 1}`,
+        answer: `Answer ${i + 1}`,
+        progress: {
+          level: newCard ? 0 : 1,
+          reviewed: newCard ? null : new Date(reviewTime - 3 * MS_PER_DAY),
+        },
+      };
+      if (!newCard) {
+        cards[i]._id = i;
+      }
+    }
+    return cards;
+  };
+
+  const cardInHistory = (card, state) => {
+    const history = state.review.history;
+    return history.some(
+      elem => elem.question === card.question && elem.answer === elem.answer
+    );
+  };
+
+  it('stores the updated review time of a passed card', async () => {
+    const reviewTime = new Date();
+    let state = reducer(undefined, reviewActions.newReview(2, 3, reviewTime));
+
+    const cards = getCards(0, 3, reviewTime);
+    state = reducer(state, reviewActions.reviewLoaded(cards));
+
+    const cardToUpdate = state.review.currentCard;
+    const action = reviewActions.passCard();
+    state = reducer(state, action);
+
+    const cardStore = { putCard: card => card };
+    return expectSaga(updateProgressSaga, cardStore, action)
+      .withState(state)
+      .call([cardStore, 'putCard'], {
+        _id: cardToUpdate._id,
+        progress: { level: cardToUpdate.progress.level, reviewed: reviewTime },
+      })
+      .run();
+  });
+
+  it('stores the updated progress of a failed card', async () => {
+    const reviewTime = new Date();
+    let state = reducer(undefined, reviewActions.newReview(1, 3, reviewTime));
+
+    const cards = getCards(0, 3, reviewTime);
+    state = reducer(state, reviewActions.reviewLoaded(cards));
+
+    const cardToUpdate = state.review.currentCard;
+    const action = reviewActions.failCard();
+    state = reducer(state, action);
+
+    const cardStore = { putCard: card => card };
+    return expectSaga(updateProgressSaga, cardStore, action)
+      .withState(state)
+      .call([cardStore, 'putCard'], {
+        _id: cardToUpdate._id,
+        progress: { level: 0, reviewed: reviewTime },
+      })
+      .run();
+  });
+
+  it('stores the updated progress of a passed card when it is the last card', async () => {
+    const reviewTime = new Date();
+    let state = reducer(undefined, reviewActions.newReview(2, 3, reviewTime));
+
+    const cards = getCards(0, 1, reviewTime);
+    state = reducer(state, reviewActions.reviewLoaded(cards));
+
+    const cardToUpdate = state.review.currentCard;
+    const action = reviewActions.passCard();
+    state = reducer(state, action);
+    assert.strictEqual(state.review.nextCard, null, 'Should have no next card');
+    assert.strictEqual(
+      state.review.currentCard,
+      null,
+      'Should have no current card'
+    );
+    assert.isTrue(
+      cardInHistory(cardToUpdate, state),
+      'Card to update is in history'
+    );
+
+    const cardStore = { putCard: card => card };
+    return expectSaga(updateProgressSaga, cardStore, action)
+      .withState(state)
+      .call([cardStore, 'putCard'], {
+        _id: cardToUpdate._id,
+        progress: { level: cardToUpdate.progress.level, reviewed: reviewTime },
+      })
+      .run();
+  });
+
+  it('stores the updated progress of a failed card when it is the last card', async () => {
+    const reviewTime = new Date();
+    let state = reducer(undefined, reviewActions.newReview(2, 3, reviewTime));
+
+    const cards = getCards(0, 2, reviewTime);
+    state = reducer(state, reviewActions.reviewLoaded(cards));
+
+    // Pass the first card so it is in history
+    state = reducer(state, reviewActions.passCard());
+
+    // Now we should have a single card left that we want to fail.
+    // We want to check we update it despite the fact that it won't go into
+    // history yet.
+    const cardToUpdate = state.review.currentCard;
+    const action = reviewActions.failCard();
+    state = reducer(state, action);
+    assert.strictEqual(state.review.nextCard, null, 'Should have no next card');
+    assert.deepEqual(
+      state.review.currentCard,
+      cardToUpdate,
+      'Card to update is the current card'
+    );
+    assert.isFalse(
+      cardInHistory(cardToUpdate, state),
+      'Card to update is not in history'
+    );
+
+    const cardStore = { putCard: card => card };
+    return expectSaga(updateProgressSaga, cardStore, action)
+      .withState(state)
+      .call([cardStore, 'putCard'], {
+        _id: cardToUpdate._id,
+        progress: { level: 0, reviewed: reviewTime },
+      })
+      .run();
   });
 });
