70fdef5c6ac587221fe19b203345725eb9c97081
Brian Birtles <birtles@gmail.com>
2017-12-11 05:16:31 -0600

    Use TabBlock in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,6 @@ import DocumentTitle from 'react-document-title';
 import { URLFromRoute } from '../router';
 import * as routeActions from '../actions/route';
 import CardOverviewScreen from './CardOverviewScreen.jsx';
-import ControlOverlay from './ControlOverlay.jsx';
 import EditCardScreen from './EditCardScreen.jsx';
 import Link from './Link.jsx';
 import Navbar from './Navbar.jsx';
@@ -14,6 +13,7 @@ import Popup from './Popup.jsx';
 import PopupOverlay from './PopupOverlay.jsx';
 import SettingsPanel from './SettingsPanel.jsx';
 import SyncSettingsPanelContainer from './SyncSettingsPanelContainer.jsx';
+import TabBlock from './TabBlock.jsx';
 
 const ConnectedNavbar =
   connect(state => ({ syncState: state.sync.state }))(Navbar);
@@ -70,6 +70,11 @@ class App extends React.Component {
       title += ` - ${toTitle(this.props.route.popup)}`;
     }
 
+    let activeTab;
+    if (this.props.route.screen === 'edit-card') {
+      activeTab = 1;
+    }
+
     return (
       <DocumentTitle title={title}>
         <div>
@@ -81,16 +86,29 @@ class App extends React.Component {
               active={!!this.props.route.popup}
               close={this.closePopup}>
               <CardOverviewScreen />
-              <ControlOverlay>
-                <button className="-primary -large -shadow -icon -review">
-                  Review
-                </button>
+              <TabBlock active={activeTab} className="tabbar">
                 <Link
+                  id="lookup-tab"
+                  href="/lookup"
+                  aria-controls="lookup-page"
+                  className="-icon -lookup">
+                  Lookup
+                </Link>
+                <Link
+                  id="edit-tab"
                   href="/cards/new"
-                  className="button -primary -large -shadow -icon -add-lookup">
+                  aria-controls="edit-page"
+                  className="-icon -plus">
                   Add
                 </Link>
-              </ControlOverlay>
+                <Link
+                  id="review-tab"
+                  href="/review"
+                  aria-controls="review-page"
+                  className="-icon -review">
+                  Review
+                </Link>
+              </TabBlock>
             </PopupOverlay>
             <Popup
               active={settingsActive}
@@ -102,6 +120,10 @@ class App extends React.Component {
             </Popup>
           </main>
           <EditCardScreen
+            id="edit-page"
+            role="tabpanel"
+            aria-labelledby="edit-tab"
+            aria-hidden={this.props.route.screen !== 'edit-card'}
             active={this.props.route.screen === 'edit-card'}
             card={this.props.route.card} />
         </div>
