972a36142f49f2f1c90ed4c3d912d76ec86355e9
Brian Birtles <birtles@gmail.com>
2017-12-29 11:56:09 +0900

    Reuse followLink action creator in tests

--- test/sagas/route.js
+++ test/sagas/route.js
@@ -12,12 +12,6 @@ import EditState from '../../src/edit-states';
 import * as routeActions from '../../src/actions/route';
 import * as editActions from '../../src/actions/edit';
 
-const followLink = (direction, url) => ({
-  type: 'FOLLOW_LINK',
-  direction,
-  url: url || '/',
-});
-
 describe('sagas:route followLink', () => {
   beforeEach('setup global', () => {
     global.history = {
@@ -28,7 +22,7 @@ describe('sagas:route followLink', () => {
   });
 
   it('does forwards navigation when direction is forwards', () => {
-    return expectSaga(followLinkSaga, followLink('forwards'))
+    return expectSaga(followLinkSaga, routeActions.followLink('/', 'forwards'))
       .provide([[matchers.call.fn(beforeScreenChangeSaga), {}]])
       .call([history, 'pushState'], { index: 0 }, '', '/')
       .put(routeActions.navigate({ url: '/' }))
@@ -36,7 +30,7 @@ describe('sagas:route followLink', () => {
   });
 
   it('does forwards navigation when direction is not specified', () => {
-    return expectSaga(followLinkSaga, followLink())
+    return expectSaga(followLinkSaga, routeActions.followLink('/'))
       .provide([[matchers.call.fn(beforeScreenChangeSaga), {}]])
       .call([history, 'pushState'], { index: 0 }, '', '/')
       .put(routeActions.navigate({ url: '/' }))
@@ -47,7 +41,7 @@ describe('sagas:route followLink', () => {
     'does forwards navigation when direction is replace but there is no' +
       ' history',
     () => {
-      return expectSaga(followLinkSaga, followLink('replace'))
+      return expectSaga(followLinkSaga, routeActions.followLink('/', 'replace'))
         .provide([[matchers.call.fn(beforeScreenChangeSaga), {}]])
         .call([history, 'pushState'], { index: 0 }, '', '/')
         .put(routeActions.navigate({ url: '/' }))
@@ -56,7 +50,10 @@ describe('sagas:route followLink', () => {
   );
 
   it('does replace navigation when direction is replace', () => {
-    return expectSaga(followLinkSaga, followLink('replace', '/?abc=123'))
+    return expectSaga(
+      followLinkSaga,
+      routeActions.followLink('/?abc=123', 'replace')
+    )
       .withState({ route: { index: 0, history: [{ screen: '/' }] } })
       .call([history, 'replaceState'], { index: 0 }, '', '/?abc=123')
       .put(routeActions.navigate({ url: '/?abc=123', replace: true }))
@@ -64,7 +61,10 @@ describe('sagas:route followLink', () => {
   });
 
   it('calls history.back() when the direction is backwards and history matches', () => {
-    return expectSaga(followLinkSaga, followLink('backwards', '/#abc'))
+    return expectSaga(
+      followLinkSaga,
+      routeActions.followLink('/#abc', 'backwards')
+    )
       .withState({
         route: {
           index: 1,
@@ -82,7 +82,10 @@ describe('sagas:route followLink', () => {
     'puts a navigate action when direction is backwards but history' +
       ' does not match because screen does not match',
     () => {
-      return expectSaga(followLinkSaga, followLink('backwards', '/settings'))
+      return expectSaga(
+        followLinkSaga,
+        routeActions.followLink('/settings', 'backwards')
+      )
         .withState({
           route: {
             index: 1,
@@ -99,7 +102,10 @@ describe('sagas:route followLink', () => {
     'puts a navigate action when direction is backwards but history' +
       ' does not match because query string does not match',
     () => {
-      return expectSaga(followLinkSaga, followLink('backwards', '/?abc=123'))
+      return expectSaga(
+        followLinkSaga,
+        routeActions.followLink('/?abc=123', 'backwards')
+      )
         .withState({
           route: {
             index: 1,
@@ -122,7 +128,10 @@ describe('sagas:route followLink', () => {
     'puts a navigate action when direction is backwards but history' +
       ' does not match because fragment does not match',
     () => {
-      return expectSaga(followLinkSaga, followLink('backwards', '/#ghi'))
+      return expectSaga(
+        followLinkSaga,
+        routeActions.followLink('/#ghi', 'backwards')
+      )
         .withState({
           route: {
             index: 1,
@@ -142,7 +151,10 @@ describe('sagas:route followLink', () => {
     'puts a navigate action when direction is backwards but history' +
       ' does not match because it is empty',
     () => {
-      return expectSaga(followLinkSaga, followLink('backwards', '/#abc'))
+      return expectSaga(
+        followLinkSaga,
+        routeActions.followLink('/#abc', 'backwards')
+      )
         .provide([[matchers.call.fn(beforeScreenChangeSaga), {}]])
         .call([history, 'pushState'], { index: 0 }, '', '/#abc')
         .put(routeActions.navigate({ url: '/#abc' }))
@@ -151,7 +163,10 @@ describe('sagas:route followLink', () => {
   );
 
   it('does nothing if the URL matches the current route and link is not active', () => {
-    return expectSaga(followLinkSaga, followLink('forwards', '/#def'))
+    return expectSaga(
+      followLinkSaga,
+      routeActions.followLink('/#def', 'forwards')
+    )
       .withState({
         route: {
           index: 1,
