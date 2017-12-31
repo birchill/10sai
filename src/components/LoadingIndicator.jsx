7e68076e437fc8a9d1bc3e673083630f2cf44e33
Brian Birtles <birtles@gmail.com>
2017-12-31 18:06:01 +0900

    Fix viewBox of loading indicator

--- src/components/LoadingIndicator.jsx
+++ src/components/LoadingIndicator.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 function LoadingIndicator() {
   return (
-    <svg className="loading-indicator" viewBox="0 0 100 100">
+    <svg className="loading-indicator" viewBox="0 0 100 30">
       <circle
         className="dot"
         cx="20"
-        cy="50"
+        cy="15"
         r="15"
       />
       <circle
         className="dot"
         cx="50"
-        cy="50"
+        cy="15"
         r="15"
       />
       <circle
         className="dot"
         cx="80"
-        cy="50"
+        cy="15"
         r="15"
       />
     </svg>
