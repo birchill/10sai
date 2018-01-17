b790d52ed170a44fa887f147986a4c5e67f926b5
Brian Birtles <birtles@gmail.com>
2018-01-17 17:32:04 +0900

    Turn on jest tests

---
+++ src/review/sync.test.js
@@ -0,0 +1,8 @@
+/* global describe, it, expect */
+
+describe('review:sync', () => {
+  // This is just to keep jest happy until we add real tests
+  it('is cool', async () => {
+    expect(true).toBe(true);
+  });
+});
