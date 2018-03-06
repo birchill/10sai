00234e0ad002590ac9aa678eaab5c7dd3969a725
Brian Birtles <birtles@gmail.com>
2018-03-06 18:06:11 +0900

    Move the review time calculation earlier so we don't end up re-generating the overdueness view twice on startup

--- src/index.js
+++ src/index.js
@@ -47,7 +47,15 @@ if (process.env.NODE_ENV === 'development') {
 // Local data stores
 //
 
-const dataStore = new DataStore();
+const getReviewTime = () => {
+  const reviewTime = new Date();
+  reviewTime.setMinutes(0);
+  reviewTime.setSeconds(0);
+  reviewTime.setMilliseconds(0);
+  return reviewTime;
+};
+
+const dataStore = new DataStore({ reviewTime: getReviewTime() });
 
 syncEditChanges(dataStore, store);
 reviewSync(dataStore, store);
@@ -127,28 +135,21 @@ window.addEventListener('offline', () => {
 //
 // Review time rotation
 //
-
-// We want to round the review time to the previous hour and then update it
-// every hour. So, for example, if we open the app at 08:49, we'll set the
-// review time to 08:00. Then, at 09:49 (NOT 09:00) we'll set the review time to
+// We round the review time to the previous hour and then update it every hour.
+// So, for example, if we open the app at 08:49, we'll set the review time to
+// 08:00. Then, at 09:49 (NOT 09:00) we'll set the review time to
 // 09:00.
 //
 // That means that if we review at roughly the same time every day any cards
 // which are marked as due precisely one day later will show up and it will also
 // prevent splitting cards reviewed at roughly the same time across different
 // review times.
-const updateReviewTime = () => {
-  const reviewTime = new Date();
-  reviewTime.setMinutes(0);
-  reviewTime.setSeconds(0);
-  reviewTime.setMilliseconds(0);
-  store.dispatch({ type: 'SET_REVIEW_TIME', reviewTime });
-};
 (() => {
   const MS_PER_HOUR = 60 * 60 * 1000;
-  setInterval(updateReviewTime, 1 * MS_PER_HOUR);
+  setInterval(() => {
+    store.dispatch({ type: 'SET_REVIEW_TIME', reviewTime: getReviewTime() });
+  }, 1 * MS_PER_HOUR);
 })();
-updateReviewTime();
 
 //
 // Render the root component
