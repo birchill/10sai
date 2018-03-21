858117689625c8e15b148eefb812acc035c80fb4
Brian Birtles <birtles@gmail.com>
2018-03-21 15:38:36 +0900

    Add Japanese text character class test functions

---
+++ src/text/japanese.test.ts
@@ -0,0 +1,65 @@
+import {
+  isHiragana,
+  isKatakana,
+  isKanji,
+  matchesCharacterClasses,
+  CharacterClass,
+} from './japanese';
+
+describe('character class checks', () => {
+  it('recognizes hiragana', () => {
+    expect(isHiragana('あ')).toEqual(true);
+    expect(isHiragana('ひらがな')).toEqual(true);
+    expect(isHiragana('Hiragana')).toEqual(false);
+    expect(isHiragana('ヒラガナ')).toEqual(false);
+    expect(isHiragana('平仮名')).toEqual(false);
+    expect(isHiragana('ひらがな。')).toEqual(false);
+  });
+
+  it('recognizes katakana', () => {
+    expect(isKatakana('ア')).toEqual(true);
+    expect(isKatakana('カタカナ')).toEqual(true);
+    expect(isKatakana('Katakana')).toEqual(false);
+    expect(isKatakana('かたかな')).toEqual(false);
+    expect(isKatakana('片仮名')).toEqual(false);
+    expect(isKatakana('カタカナ。')).toEqual(false);
+    expect(isKatakana('ｶﾀｶﾅ')).toEqual(false);
+  });
+
+  it('recognizes half-width katakana', () => {
+    expect(
+      matchesCharacterClasses('ｱ', CharacterClass.HalfWidthKatakana)
+    ).toEqual(true);
+    expect(
+      matchesCharacterClasses('ｶﾀｶﾅ', CharacterClass.HalfWidthKatakana)
+    ).toEqual(true);
+    expect(
+      matchesCharacterClasses('カタカナ', CharacterClass.HalfWidthKatakana)
+    ).toEqual(false);
+  });
+
+  it('recognizes kanji', () => {
+    expect(isKanji('字')).toEqual(true);
+    expect(isKanji('漢字')).toEqual(true);
+    expect(isKanji('かんじ')).toEqual(false);
+    expect(isKanji('カンジ')).toEqual(false);
+    expect(isKanji('Kanji')).toEqual(false);
+    expect(isKanji('漢字。')).toEqual(false);
+    expect(isKanji('𠁎')).toEqual(true);
+  });
+
+  it('allows combining classes', () => {
+    expect(
+      matchesCharacterClasses(
+        '漢字マン',
+        CharacterClass.Kanji | CharacterClass.Katakana
+      )
+    ).toEqual(true);
+    expect(
+      matchesCharacterClasses(
+        '漢字だよ',
+        CharacterClass.Kanji | CharacterClass.Katakana
+      )
+    ).toEqual(false);
+  });
+});
---
+++ src/text/japanese.ts
@@ -0,0 +1,76 @@
+export const enum CharacterClass {
+  Hiragana = 1 << 0,
+  Katakana = 1 << 1,
+  Kana = Hiragana | Katakana,
+  Kanji = 1 << 2,
+  HalfWidthKatakana = 1 << 3,
+  Hentaigana = 1 << 4,
+  _Max = 1 << 5,
+}
+
+export function matchesCharacterClasses(text: string, flags: number): boolean {
+  if (flags < 0 || flags >= CharacterClass._Max) {
+    throw new Error('Invalid character class');
+  }
+
+  // Treat an empty string as not matching anything
+  if (!text.length) {
+    return false;
+  }
+
+  return [...text].map(c => c.codePointAt(0)!).every(c => {
+    if (
+      flags & CharacterClass.Hiragana &&
+      ((c >= 0x3040 && c <= 0x309f) || c === 0x1b001)
+    ) {
+      return true;
+    }
+
+    if (
+      flags & CharacterClass.Katakana &&
+      ((c >= 0x30a0 && c <= 0x30ff) ||
+        (c >= 0x31f0 && c <= 0x31ff) ||
+        c === 0x1b000)
+    ) {
+      return true;
+    }
+
+    if (
+      flags & CharacterClass.Kanji &&
+      ((c >= 0x4e00 && c <= 0x9fea) ||
+      (c >= 0x3400 && c <= 0x4dbf) /* Ideographs extension A */ ||
+        (c >= 0x20000 && c <= 0x2ebef)) /* Ideographs extension B&C&D&E */
+    ) {
+      return true;
+    }
+
+    if (
+      flags & CharacterClass.HalfWidthKatakana &&
+      (c >= 0xff65 && c <= 0xff9f)
+    ) {
+      return true;
+    }
+
+    if (flags & CharacterClass.Hentaigana && c >= 0x1b002 && c <= 0x1b0ff) {
+      return true;
+    }
+
+    return false;
+  });
+}
+
+export function isHiragana(text: string): boolean {
+  return matchesCharacterClasses(text, CharacterClass.Hiragana);
+}
+
+export function isKatakana(text: string): boolean {
+  return matchesCharacterClasses(text, CharacterClass.Katakana);
+}
+
+export function isKana(text: string): boolean {
+  return matchesCharacterClasses(text, CharacterClass.Kana);
+}
+
+export function isKanji(text: string): boolean {
+  return matchesCharacterClasses(text, CharacterClass.Kanji);
+}
