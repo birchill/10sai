4b18f424af7b0523f003f951ce276e7751ea0588
Brian Birtles <birtles@gmail.com>
2018-07-19 13:24:30 +0900

    Add a few more tests for NoteListWatcher

--- src/notes/NoteListWatcher.test.ts
+++ src/notes/NoteListWatcher.test.ts
@@ -102,7 +102,7 @@ describe('NoteListWatcher', () => {
     expect(await subject.getNotes()).toEqual([note1, note2]);
   });
 
-  it('does NOT report a newly-added card that does NOT matches', async () => {
+  it('does NOT report a newly-added card that does NOT match', async () => {
     const note1 = await dataStore.putNote({
       content: 'Note 1',
       keywords: ['ABC', 'DEF'],
@@ -112,7 +112,6 @@ describe('NoteListWatcher', () => {
 
     const [callback] = waitForCalls(1);
     const subject = new NoteListWatcher(dataStore, callback, ['ABC']);
-
     await subject.getNotes();
 
     const note2 = await dataStore.putNote({
@@ -123,12 +122,158 @@ describe('NoteListWatcher', () => {
     expect(await subject.getNotes()).toEqual([note1]);
   });
 
-  // Test: A deleted card that did match
-  // Test: A deleted card that does not match
-  // Test: A content change to a card that does match
-  // Test: A content change to a card that does not match
-  // Test: A keyword change to a card so that it begins to matches
-  // Test: A keyword change to a card so that it no longer matches
+  it('reports a deleted card that matches', async () => {
+    const note = await dataStore.putNote({
+      content: 'Note',
+      keywords: ['abc', 'def'],
+    });
+
+    const result: Array<Note> = [];
+
+    const [callback, finished] = waitForCalls(2);
+    const subject = new NoteListWatcher(dataStore, callback, ['ABC']);
+    await subject.getNotes();
+
+    await dataStore.deleteNote(note.id);
+
+    const calls = await finished;
+
+    expect(calls[0]).toEqual([note]);
+    expect(calls[1]).toEqual([]);
+    expect(await subject.getNotes()).toEqual([]);
+  });
+
+  it('does NOT report a deleted card that does NOT match', async () => {
+    const note1 = await dataStore.putNote({
+      content: 'Note 1',
+      keywords: ['abc'],
+    });
+    const note2 = await dataStore.putNote({
+      content: 'Note 2',
+      keywords: ['def'],
+    });
+
+    const result: Array<Note> = [];
+
+    const [callback, finished] = waitForCalls(1);
+    const subject = new NoteListWatcher(dataStore, callback, ['ABC']);
+    await subject.getNotes();
+
+    await dataStore.deleteNote(note2.id);
+
+    const calls = await finished;
+
+    expect(calls[0]).toEqual([note1]);
+    expect(await subject.getNotes()).toEqual([note1]);
+  });
+
+  it('reports a content change to a card that matches', async () => {
+    const note = await dataStore.putNote({
+      content: 'Content',
+      keywords: ['ABC', 'DEF'],
+    });
+
+    const result: Array<Note> = [];
+
+    const [callback, finished] = waitForCalls(2);
+    const subject = new NoteListWatcher(dataStore, callback, ['ABC']);
+    await subject.getNotes();
+
+    const updatedNote = await dataStore.putNote({
+      ...note,
+      content: 'Updated content',
+    });
+
+    const calls = await finished;
+
+    expect(calls[0]).toEqual([note]);
+    expect(calls[1]).toEqual([updatedNote]);
+    expect(await subject.getNotes()).toEqual([updatedNote]);
+  });
+
+  it('does NOT report a content change to a card that does NOT match', async () => {
+    const note1 = await dataStore.putNote({
+      content: 'Note 1',
+      keywords: ['ABC'],
+    });
+    const note2 = await dataStore.putNote({
+      content: 'Note 2',
+      keywords: ['DEF'],
+    });
+
+    const result: Array<Note> = [];
+
+    const [callback, finished] = waitForCalls(1);
+    const subject = new NoteListWatcher(dataStore, callback, ['ABC']);
+    await subject.getNotes();
+
+    const updatedNote2 = await dataStore.putNote({
+      ...note2,
+      content: 'Updated content',
+    });
+
+    const calls = await finished;
+
+    expect(calls[0]).toEqual([note1]);
+    expect(await subject.getNotes()).toEqual([note1]);
+  });
+
+  it('reports an updated card that begins to match', async () => {
+    const note1 = await dataStore.putNote({
+      content: 'Note 1',
+      keywords: ['ABC'],
+    });
+    const note2 = await dataStore.putNote({
+      content: 'Note 2',
+      keywords: ['DEF'],
+    });
+
+    const result: Array<Note> = [];
+
+    const [callback, finished] = waitForCalls(2);
+    const subject = new NoteListWatcher(dataStore, callback, ['ABC']);
+    await subject.getNotes();
+
+    const updatedNote2 = await dataStore.putNote({
+      ...note2,
+      keywords: ['Abc', 'DEF'],
+    });
+
+    const calls = await finished;
+
+    expect(calls[0]).toEqual([note1]);
+    expect(calls[1]).toEqual([note1, updatedNote2]);
+    expect(await subject.getNotes()).toEqual([note1, updatedNote2]);
+  });
+
+  it('reports an updated card that stops matching', async () => {
+    const note1 = await dataStore.putNote({
+      content: 'Note 1',
+      keywords: ['ABC'],
+    });
+    const note2 = await dataStore.putNote({
+      content: 'Note 2',
+      keywords: ['ABC', 'DEF'],
+    });
+
+    const result: Array<Note> = [];
+
+    const [callback, finished] = waitForCalls(2);
+    const subject = new NoteListWatcher(dataStore, callback, ['abc']);
+    await subject.getNotes();
+
+    await dataStore.putNote({
+      ...note2,
+      keywords: ['DEF'],
+    });
+
+    const calls = await finished;
+
+    expect(calls[0]).toEqual([note1, note2]);
+    expect(calls[1]).toEqual([note1]);
+    expect(await subject.getNotes()).toEqual([note1]);
+  });
+
   // Test: A keyword change to a card so that it continues to match
   // Test: A keyword change to a card so that it continues to not match
   // Test: A redundant change on a card that does match
