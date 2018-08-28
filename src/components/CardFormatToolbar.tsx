651fba7178bed013d24a5d983208bf9ea4ead9d0
Brian Birtles <birtles@gmail.com>
2018-08-28 11:04:34 +0900

    Add keyboard handling for formatting toolbar

--- src/components/CardFormatToolbar.tsx
+++ src/components/CardFormatToolbar.tsx
@@ -6,9 +6,24 @@ interface Props {
   onClick: (command: string) => void;
 }
 
-export class CardFormatToolbar extends React.PureComponent<Props> {
-  containerRef: React.RefObject<HTMLDivElement>;
+interface State {
+  focusIndex: number;
+}
 
+type Button = {
+  action: string;
+  title: string;
+  accelerator: string;
+};
+
+const buttons: Array<Button> = [
+  { action: 'bold', title: 'Bold', accelerator: 'Ctrl+B' },
+  { action: 'italic', title: 'Italic', accelerator: 'Ctrl+I' },
+  { action: 'underline', title: 'Underline', accelerator: 'Ctrl+U' },
+  { action: 'emphasis', title: 'Dot emphasis', accelerator: 'Ctrl+.' },
+];
+
+export class CardFormatToolbar extends React.PureComponent<Props, State> {
   static get propTypes() {
     return {
       className: PropTypes.string,
@@ -16,12 +31,19 @@ export class CardFormatToolbar extends React.PureComponent<Props> {
     };
   }
 
+  containerRef: React.RefObject<HTMLDivElement>;
+  state: State;
+
   constructor(props: Props) {
     super(props);
 
+    this.state = {
+      focusIndex: 0,
+    };
     this.containerRef = React.createRef<HTMLDivElement>();
 
     this.handleClick = this.handleClick.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
   handleClick(evt: React.MouseEvent<HTMLButtonElement>) {
@@ -29,6 +51,43 @@ export class CardFormatToolbar extends React.PureComponent<Props> {
       const action = (evt.target as HTMLButtonElement).dataset.action!;
       this.props.onClick(action);
     }
+  }
+
+  handleKeyDown(evt: React.KeyboardEvent<HTMLDivElement>) {
+    switch (evt.key) {
+      case 'ArrowLeft':
+        if (this.state.focusIndex) {
+          this.setState({ focusIndex: this.state.focusIndex - 1 }, () =>
+            this.updateFocus()
+          );
+        }
+        evt.preventDefault();
+        return;
+
+      case 'ArrowRight':
+        if (this.state.focusIndex < buttons.length - 1) {
+          this.setState({ focusIndex: this.state.focusIndex + 1 }, () =>
+            this.updateFocus()
+          );
+        }
+        evt.preventDefault();
+        return;
+
+      default:
+        return;
+    }
+  }
+
+  updateFocus() {
+    if (!this.containerRef.current) {
+      return;
+    }
+
+    const getNthButton = (i: number) =>
+      this.containerRef.current!.querySelector(
+        `button:nth-of-type(${i + 1})`
+      ) as HTMLButtonElement;
+    getNthButton(this.state.focusIndex).focus();
   }
 
   get element(): HTMLElement | null {
@@ -42,43 +101,24 @@ export class CardFormatToolbar extends React.PureComponent<Props> {
     }
 
     return (
-      <div className={className} ref={this.containerRef}>
-        <button
-          className="bold button -icon"
-          onClick={this.handleClick}
-          title="Bold (Ctrl+B)"
-          data-action="bold"
-          type="button"
-        >
-          Bold
-        </button>
-        <button
-          className="italic button -icon"
-          onClick={this.handleClick}
-          title="Italic (Ctrl+I)"
-          data-action="italic"
-          type="button"
-        >
-          Italic
-        </button>
-        <button
-          className="underline button -icon"
-          onClick={this.handleClick}
-          title="Underline (Ctrl+U)"
-          data-action="underline"
-          type="button"
-        >
-          Underline
-        </button>
-        <button
-          className="emphasis button -icon"
-          onClick={this.handleClick}
-          title="Dot emphasis (Ctrl+.)"
-          data-action="emphasis"
-          type="button"
-        >
-          Emphasis
-        </button>
+      <div
+        className={className}
+        ref={this.containerRef}
+        onKeyDown={this.handleKeyDown}
+      >
+        {buttons.map((button, i) => (
+          <button
+            key={button.action}
+            className={`${button.action} button -icon`}
+            onClick={this.handleClick}
+            title={`${button.title} (${button.accelerator})`}
+            data-action={button.action}
+            type="button"
+            tabIndex={i === this.state.focusIndex ? 0 : -1}
+          >
+            {button.title}
+          </button>
+        ))}
       </div>
     );
   }
