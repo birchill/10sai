04aeb7dc4ab8eef4b0dfa6f0e50adf91d45d1989
Brian Birtles <birtles@gmail.com>
2018-09-15 13:30:06 +0900

    Convert CardPreview to a PureComponent

--- src/components/CardPreview.tsx
+++ src/components/CardPreview.tsx
@@ -9,62 +9,72 @@ interface Props {
   question: string;
 }
 
-export const CardPreview: React.SFC<Props> = props => {
-  // If this proves slow we can try to either:
-  //
-  // - Memoize the result of deserialize (if that is the bottleneck)
-  // - Possibly even do bulk parsing and rendering in a WASM module, have it
-  //   spit out HTML strings and use dangerouslySetInnerHTML on them.
-  //
-  // For now, though, it seems acceptable.
-  const blocks = deserialize(props.question);
-  let i = 0;
+// We make this a pure component simply to avoid calling deserialize unless the
+// question changes.
+export class CardPreview extends React.PureComponent<Props> {
+  static get propTypes() {
+    return {
+      question: PropTypes.string.isRequired,
+    };
+  }
 
-  // I thought flexbox was supposed to fix all the problems with CSS but
-  // we still have to add an extra div just to use it :/
-  return (
-    <div className="card-preview">
-      <div className="flex-container">
-        <span className="question">
-          {blocks.map(block => {
-            const key = `block-${i++}`;
-            return <p key={key}>{renderChildren(block.children, key)}</p>;
-          })}
-        </span>
+  render() {
+    // If this proves slow we can try to either:
+    //
+    // - Memoize the result of deserialize (if that is the bottleneck)
+    // - Possibly even do bulk parsing and rendering in a WASM module, have it
+    //   spit out HTML strings and use dangerouslySetInnerHTML on them.
+    //
+    // For now, though, it seems acceptable.
+    const blocks = deserialize(this.props.question);
+    let i = 0;
+
+    // I thought flexbox was supposed to fix all the problems with CSS but
+    // we still have to add an extra div just to use it :/
+    return (
+      <div className="card-preview">
+        <div className="flex-container">
+          <span className="question">
+            {blocks.map(block => {
+              const key = `block-${i++}`;
+              return (
+                <p key={key}>
+                  {CardPreview.renderChildren(block.children, key)}
+                </p>
+              );
+            })}
+          </span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
 
-CardPreview.propTypes = {
-  question: PropTypes.string.isRequired,
-};
+  static renderChildren(
+    children: Array<Inline | string>,
+    parentKey: string
+  ): React.ReactNode {
+    let i = 0;
 
-function renderChildren(
-  children: Array<Inline | string>,
-  parentKey: string
-): React.ReactNode {
-  let i = 0;
-
-  return (
-    <>
-      {children.map(child => {
-        const key = `${parentKey}-${i++}`;
-        if (typeof child === 'string') {
-          return stripRuby(child);
-        } else {
-          const className = child.styles
-            .map(style => styleClassMapping.get(style))
-            .join(' ');
-          return (
-            <span key={key} className={className}>
-              {renderChildren(child.children, key)}
-            </span>
-          );
-        }
-      })}
-    </>
-  );
+    return (
+      <>
+        {children.map(child => {
+          const key = `${parentKey}-${i++}`;
+          if (typeof child === 'string') {
+            return stripRuby(child);
+          } else {
+            const className = child.styles
+              .map(style => styleClassMapping.get(style))
+              .join(' ');
+            return (
+              <span key={key} className={className}>
+                {CardPreview.renderChildren(child.children, key)}
+              </span>
+            );
+          }
+        })}
+      </>
+    );
+  }
 }
 
 export default CardPreview;
