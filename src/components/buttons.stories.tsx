0297fcd0cc92e7dd02029c40ae9731ac44d606ff
Brian Birtles <birtles@gmail.com>
2018-09-17 21:47:56 +0900

    Finish converting button preview to storybook

--- src/components/buttons.stories.tsx
+++ src/components/buttons.stories.tsx
@@ -4,14 +4,55 @@ import { storiesOf } from '@storybook/react';
 storiesOf('Forms|Buttons', module)
   .add('default', () => (
     <>
-      <input type="submit" value="&lt;input type=&quot;submit&quot;&gt;" />
-      <input type="button" value="&lt;input type=&quot;button&quot;&gt;" />
-      <button>&lt;button&gt;</button>
-      <a className="button" role="button">
-        &lt;a class="button"&gt;
-      </a>
+      <div className="row">
+        <input type="submit" value="&lt;input type=&quot;submit&quot;&gt;" />
+        <input type="button" value="&lt;input type=&quot;button&quot;&gt;" />
+        <button>&lt;button&gt;</button>
+        <a className="button" role="button">
+          &lt;a class="button"&gt;
+        </a>
+      </div>
+      <div className="row">
+        <input type="button" disabled value="disabled" />
+        <input type="button" className="-primary" value="-primary" />
+        <input
+          type="button"
+          disabled
+          className="-primary"
+          value="disabled -primary"
+        />
+        <input type="button" className="-link" value="-link" />
+        <input type="button" className="-large" value="-large" />
+      </div>
+      <div className="row">
+        <button className="-icon -delete">-icon -delete</button>
+        <button className="-icon -plus">-icon -plus</button>
+        <button className="-icon -edit-card">-icon -edit-card</button>
+        <button className="-icon -delete" disabled>
+          -icon -delete disabled
+        </button>
+      </div>
+      <div className="row">
+        <button className="-icon -delete -large">-icon -delete -large</button>
+        <button className="-icon -plus -large -primary">
+          -icon -plus -large -primary
+        </button>
+        <button className="-icon -add-card -link">-icon -add-card -link</button>
+      </div>
     </>
   ))
   .add('primary', () => (
-    <input type="button" className="-primary" value="-primary" />
+    <div className="row">
+      <input type="button" className="-primary" value="-primary" />
+      <input
+        type="button"
+        className="-primary"
+        value="disabled -primary"
+        disabled
+      />
+      <button className="-icon -plus -primary">-icon -plus -primary</button>
+      <button className="-icon -plus -large -primary">
+        -icon -plus -large -primary
+      </button>
+    </div>
   ));
