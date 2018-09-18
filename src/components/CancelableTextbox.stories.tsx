c6d217f9059504a0b3eb571f87b0b9477c4a0b08
Brian Birtles <birtles@gmail.com>
2018-09-18 09:19:26 +0900

    Introduce addon-actions for storybook

--- src/components/CancelableTextbox.stories.tsx
+++ src/components/CancelableTextbox.stories.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { CancelableTextbox } from './CancelableTextbox';
 
 storiesOf('Components|CancelableTextbox', module).add('default', () => (
   <CancelableTextbox
     value="CancelableTextbox"
-    onChange={() => {
-      console.log('CancelableTextbox: onChange');
-    }}
-    onFocus={() => {
-      console.log('CancelableTextbox: onFocus');
-    }}
+    onChange={action('onChange')}
+    onFocus={action('onFocus')}
   />
 ));
