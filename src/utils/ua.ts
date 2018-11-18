433f922f9dd4a3d11ffdc14a7f3e7efba2c81ed7
Brian Birtles <birtles@gmail.com>
2018-11-18 21:05:01 +0900

    Add missing ua.ts

---
+++ src/utils/ua.ts
@@ -0,0 +1,7 @@
+import UAParser from 'ua-parser-js';
+
+const parser = new UAParser();
+const os = parser.getOS();
+
+export const isWindows = os.name === 'Windows';
+export const isMac = os.name === 'Mac OS';
