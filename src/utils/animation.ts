14c759add6482bc31c400e20bd8f1bdc4efb8236
Brian Birtles <birtles@gmail.com>
2018-12-03 21:43:40 -0500

    Switch to animation event listeners

--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -4,27 +4,19 @@ export const getFinishedPromise = (anim: Animation): Promise<Animation> => {
   }
 
   return new Promise((resolve, reject) => {
-    /* XXX Upgrade to TS 3.0.1 and then use the following...
-      anim.addEventListener(
-        'finish',
-        () => {
-          resolve(anim);
-        },
-        { once: true }
-      );
-      anim.addEventListener(
-        'cancel',
-        () => {
-          reject(new DOMException('Aborted', 'AbortError'));
-        },
-        { once: true }
-      );
-    */
-    anim.onfinish = () => {
-      resolve(anim);
-    };
-    anim.oncancel = () => {
-      reject(new DOMException('Aborted', 'AbortError'));
-    };
+    anim.addEventListener(
+      'finish',
+      () => {
+        resolve(anim);
+      },
+      { once: true }
+    );
+    anim.addEventListener(
+      'cancel',
+      () => {
+        reject(new DOMException('Aborted', 'AbortError'));
+      },
+      { once: true }
+    );
   });
 };
