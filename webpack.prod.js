c863bc9e0d2cabc6e5b33552f62332e36008f6cb
Brian Birtles <birtles@gmail.com>
2018-12-05 09:11:14 -0500

    Switch from uglify to terser

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,11 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
-  plugins: [
-    new UglifyJsPlugin({
-      uglifyOptions: {
-        ecma: 6,
-      },
-      sourceMap: true,
-    }),
-  ],
+  optimization: {
+    minimizer: [new TerserPlugin({ sourceMap: true })],
+  },
 });
