a9c6c8c21f18ca5a7da2cd7a5a7648e13561bd73
Brian Birtles <birtles@gmail.com>
2019-06-10 16:27:11 +0900

    Switch from extract-text-webpack-plugin to mini-css-extract-plugin

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,4 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = ({ config, mode }) => {
@@ -13,23 +13,43 @@ module.exports = ({ config, mode }) => {
   // SASS
   config.module.rules.push({
     test: /\.scss$/,
-    use: ExtractTextPlugin.extract({
-      use: [
-        { loader: 'raw-loader' },
-        {
-          loader: 'sass-loader',
-          options: {
-            includePaths: [path.resolve(__dirname, '../scss')],
-            sourceMap: true,
-          },
+    use: [
+      {
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+          publicPath: '../public/',
+          hmr: process.env.NODE_ENV === 'development',
         },
-      ],
-    }),
+      },
+      {
+        loader: 'css-loader',
+        options: {
+          url: false,
+        },
+      },
+      {
+        loader: 'sass-loader',
+        options: {
+          includePaths: [path.resolve(__dirname, '../scss')],
+          sourceMap: true,
+        },
+      },
+    ],
   });
   config.resolve.modules.push(path.resolve(__dirname, '../scss'));
-  config.plugins.push(
-    new ExtractTextPlugin({ filename: 'storybook.css', allChunks: true })
-  );
+  config.plugins.push(new MiniCssExtractPlugin({ filename: 'storybook.css' }));
+  config.optimization = {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  };
 
   return config;
 };
