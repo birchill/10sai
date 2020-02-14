2079f990e91640403e26a50922e1284a05d34519
Brian Birtles <birtles@gmail.com>
2020-02-14 13:25:31 +0900

    Merge ReviewPhase.Front/Back into a single Reviewing state

--- src/review/review-phase.ts
+++ src/review/review-phase.ts
@@ -2,7 +2,6 @@ export const enum ReviewPhase {
   Idle = 'idle', // No review in progress
   Loading = 'loading', // Currently loading cards for review or
   // performing initial load
-  Front = 'front', // Showing the front of a card
-  Back = 'back', // Showing the back of a card
+  Reviewing = 'reviewing', // Showing the front of a card
   Complete = 'complete', // Review is finished
 }
